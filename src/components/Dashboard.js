c1d390923d855df733eeacd246775d69f75e488f
ArturT <mr.arturio@gmail.com>
2023-06-15 22:56:51 -0400

    panel component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
 
 import Loading from './Loading';
+import Panel from "./Panel";
 import classnames from "classnames";
 
+//mock data
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
+data.map()
 class Dashboard extends Component {
-  state = { 
-    loading: false 
+  state = {
+    loading: false
   };
 
   render() {
@@ -15,7 +41,16 @@ class Dashboard extends Component {
       return <Loading />;
     }
 
-    return <main className={dashboardClasses} />;
+    const panels = data.map(panel => (
+      <Panel
+        key={panel.id}
+        id={panel.id}
+        label={panel.label}
+        value={panel.value}
+      />
+    ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
